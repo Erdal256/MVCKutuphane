4d9787f21c0f13ac44db178f58e98a7027dd3b08
Erdal256 <erdall.2693@gmail.com>
2021-03-21 15:28:44 +0300

    Add project files.

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+namespace MVCKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        [HttpGet] //sayfa yüklendiğinde bu kısım çalışsın. başka bir şey yapmasın
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        //TBLKATEGORİDEN türediği için p parametresini ekledik.
+        //kategorilerden Add View diyerek kategoriekle türettik.
+        [HttpPost] // sayfa yülendiğinde bir kısıma tıkladığımızda çalışması için bu kısım çalışsın.
+        public ActionResult KategoriEkle(TBLKATEGORI p) 
+        {
+            db.TBLKATEGORI.Add(p);//eklemek p den gelen degerleri ekle
+            db.SaveChanges(); //değişiklikleri kaydet demek.
+            return View(); //sayfayı bize geri döndürecektir.
+        }
+    }
+}
---
+++ Controllers/KitapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+namespace MVCKutuphane.Controllers
+{
+    public class KitapController : Controller
+    {
+        //// GET: Kitap
+        //DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        //public ActionResult Index()
+        //{
+        //    var degerler = db.TBLKATEGORI.ToList();
+        //    return View(degerler);
+        //}
+    }
+}

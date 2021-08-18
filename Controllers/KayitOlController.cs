e57935980dcb50446c881ce3ebdc96a091efcecd
Erdal256 <erdall.2693@gmail.com>
2021-08-19 00:15:30 +0300

    V9

---
+++ Controllers/KayitOlController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+
+namespace MVCKutuphane.Controllers
+{
+  
+    public class KayitOlController : Controller
+    {
+        // GET: KayitOl
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        [HttpGet]
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kayit(TBLUYELER p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Kayit");
+            }
+            db.TBLUYELER.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
--- Controllers/VitrinController.cs
+++ Controllers/VitrinController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+using MVCKutuphane.Models.Siniflarim;
 
 namespace MVCKutuphane.Controllers
 {
     public class VitrinController : Controller
     {
         // GET: Vitrin
+        DBKUTUPHANEEntities1 db = new DBKUTUPHANEEntities1();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            Class1 cs = new Class1();
+            cs.Deger1 = db.TBLKITAP.ToList();
+            cs.Deger2 = db.TBLHAKKIMIZDA.ToList();
+            //var degerler = db.TBLKITAP.ToList();
+            return View(cs);
+        }
+        [HttpPost]
+        public ActionResult Index(TBLILETISIM t)
+        {
+            db.TBLILETISIM.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
